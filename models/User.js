f391fc01027aae0bb6089169de61c1048152ef8f
Andreas Stopat <dev@demonsgalore.de>
2019-04-02 11:37:46 +0200

    Add models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
